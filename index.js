6e350350434b0d2255c1827b7794644d0edfe8f7
Simon Monecke <SimonMonecke@gmail.com>
2017-11-17 18:53:59 +0100

    remove fit-to-screen mode, fix buckets in default mode, fix axis, edit cmd-usage, improve args handling

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const args = require('./lib/cli')();
-const simpleGit = require('simple-git')(process.argv[2])
+const simpleGit = require('simple-git')
 const moment = require('moment');
 
 const screen = require('blessed').screen();
@@ -8,14 +8,17 @@ const splashscreen = require('./lib/gui/splashscreen');
 const header = require('./lib/gui/header');
 const footer = require('./lib/gui/footer');
 const chart = require('./lib/gui/chart');
-var granularitiesOrder = ['days', 'weeks', 'months'];
-var gitLog = [];
-var fitToScreen = true;
-var buckets = [];
+let repo;
+let gitLog = [];
+let since;
+let until;
 
-header.setGranularity(granularitiesOrder[0]);
-header.setFromDate('2014-11-01');
-header.setToDate('2016-02-31');
+try {
+  repo = simpleGit(args.directory);
+} catch(err) {
+  console.log("Cannot use git-commit-chart on a directory that does not exist");
+  process.exit(0);
+}
 
 screen.append(splashscreen.getBlessedComponent());
 screen.render();
@@ -24,95 +27,50 @@ screen.append(header.getBlessedComponent());
 screen.append(chart.getBlessedComponent());
 screen.append(footer.getBlessedComponent());
 
-function recalculateBucketsAndUpdateChart() {
-  if (gitLog.length > 0) {
-    let since = args.since;
-    let until = args.until ? moment(args.until) : moment();
-    buckets = {
-      x: [],
-      y: []
-    };
-    for (let i = 0; i < gitLog.length; i++) {
-      let currentDiff = until.diff(gitLog[i], granularitiesOrder[0]);
-      if (!!buckets.y[currentDiff]) {
-        buckets.y[currentDiff]++;
-      } else {
-        buckets.y[currentDiff] = 1;
-      }
-    }
-    for (let i = 0; i < buckets.y.length; i++) {
-      if (!buckets.y[i]) {
-        buckets.y[i] = 0;
-      }
-      buckets.x.push(moment(until).subtract(i, granularitiesOrder[0]));
-    }
-    buckets.x = buckets.x.reverse();
-    buckets.y = buckets.y.reverse();
-    updateChart();
-  }
+var gitLogOptions = {};
+if (!!args.since) {
+  since = args.since;
+  gitLogOptions['--since'] = since.format('YYYY-MM-DD');
 }
+if (!!args.until) {
+  until = args.until;
+  gitLogOptions['--until'] = until.format('YYYY-MM-DD');
+}
+
+repo.revparse(['--show-toplevel'], function (err, data) {
+  header.setRepoName(path.basename(data).trim());
+});
 
 function updateChart() {
   chart.getBlessedComponent().setData({
-    fitToScreen: fitToScreen,
-    x: buckets.x,
-    y: buckets.y
+    gitLog: gitLog.reverse(),
+    since: since,
+    until: until,
   });
   screen.render();
 }
 
-var gitLogOptions = {};
-if (args.since) {
-  gitLogOptions['--since'] = moment(args.since).format('YYYY-MM-DD');
-}
-if (args.until) {
-  gitLogOptions['--until'] = moment(args.untils).format('YYYY-MM-DD');
-}
-
-simpleGit.revparse(['--show-toplevel'], function(err, data) {
-  header.setRepoName(path.basename(data).trim());
-});
-
-simpleGit.log(gitLogOptions, function (err, data) {
-  let since = args.since;
-  let until = args.until ? moment(args.until) : moment();
+repo.log(gitLogOptions, function (err, data) {
   for (let i = 0; i < data.all.length; i++) {
     let currentDate = moment(new Date(data.all[i].date));
-    if ((!since || currentDate.isAfter(since)) && currentDate.isBefore(until)) {
-      gitLog.push(currentDate);
-    }
+    gitLog.push(currentDate);
   }
-  recalculateBucketsAndUpdateChart();
+  if(gitLog.length === 0) {
+    screen.destroy();
+    console.log("Cannot find any commits");
+    process.exit(0);
+  }
+  if (!since || gitLog[gitLog.length - 1].isAfter(since)) {
+    since = gitLog[gitLog.length - 1];
+  }
+  if (!until || gitLog[0].isBefore(until)) {
+    until = gitLog[0];
+  }
+  header.setSince(since.format('YYYY-MM-DD'));
+  header.setUntil(until.format('YYYY-MM-DD'));
+  updateChart();
 });
 
-screen.key('g', function() {
-  granularitiesOrder.push(granularitiesOrder.shift());
-  header.setGranularity(granularitiesOrder[0]);
-  recalculateBucketsAndUpdateChart();
-  const contentFits = chart.getBlessedComponent().contentFits();
-  footer.showScrollKeys(!contentFits);
-  screen.render();
-});
-
-screen.key('f', function() {
-  fitToScreen = !fitToScreen;
-  header.toggleFitToScreen();
-  recalculateBucketsAndUpdateChart();
-  const contentFits = chart.getBlessedComponent().contentFits();
-  footer.showScrollKeys(!contentFits);
-  screen.render();
-});
-
-screen.key('left', function() {
-  chart.getBlessedComponent().scrollLeft();
-  screen.render();
-});
-
-screen.key('right', function() {
-  chart.getBlessedComponent().scrollRight();
-  screen.render();
-});
-
-screen.key('q', function() {
+screen.key('q', function () {
   process.exit(0);
 });
